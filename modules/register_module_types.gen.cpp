c0dad33f23a125b7b95d75e05fca2381689a826d
lijiejun <9890123@qq.com>
2022-01-05 15:16:40 +0800

    2022/01/05

--- modules/register_module_types.gen.cpp
+++ modules/register_module_types.gen.cpp
@@ -4,16 +4,28 @@
 
 #include "modules/modules_enabled.gen.h"
 
+#include "modules/freetype/register_types.h"
 
 
 void preregister_module_types() {
+#ifdef MODULE_FREETYPE_ENABLED
+#ifdef MODULE_FREETYPE_HAS_PREREGISTER
+	preregister_freetype_types();
+#endif
+#endif
 
 }
 
 void register_module_types() {
+#ifdef MODULE_FREETYPE_ENABLED
+	register_freetype_types();
+#endif
 
 }
 
 void unregister_module_types() {
+#ifdef MODULE_FREETYPE_ENABLED
+	unregister_freetype_types();
+#endif
 
 }
